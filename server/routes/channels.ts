a44da88af0d813c60a4c1fec13fef9c4fd29b44d
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-03-21 18:15:38 +0100

    🚚 Add channelController and update channels route

--- server/routes/channels.ts
+++ server/routes/channels.ts
@@ -5,6 +5,7 @@ import authenticateUser from "../middlewares/authenticateUser";
 const channelRouter = express.Router();
 
 channelRouter.post("/", authenticateUser, ChannelController.create);
+channelRouter.delete("/", authenticateUser, ChannelController.clear);
 channelRouter.delete("/:channel_id", ChannelController.delete);
 
 export default channelRouter;
